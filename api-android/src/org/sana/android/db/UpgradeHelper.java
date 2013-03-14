dd567564e30ec0524b511182a0f88613090e37db
Eric Winkler <winkler.em@gmail.com>
2013-03-14 15:04:11 -0400

    Helper interfaces for the database.

---
+++ api-android/src/org/sana/android/db/UpgradeHelper.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2013, Sana
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Sana nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL Sana BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.sana.android.db;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * @author Sana Development
+ *
+ */
+public interface UpgradeHelper {
+	/**
+	 * Upgrades a table if the version has been incremented. 
+	 * @param db THe database to upgrade the table in.
+	 * @param oldVersion The current version of the table in the database.
+	 * @param newVersion The version it will be upgraded to.
+	 */
+	void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) ;
+}
