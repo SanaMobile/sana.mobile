b35fba94d2f438ccf24bebeb2b56fea673a6a0c9
Eric Winkler <winkler.em@gmail.com>
2013-04-24 15:34:11 -0400

    Added session and instance key fields.

--- app-android/src/org/sana/android/activity/BaseActivity.java
+++ app-android/src/org/sana/android/activity/BaseActivity.java
@@ -1,5 +1,7 @@
 package org.sana.android.activity;
 
+import java.util.UUID;
+
 import android.content.Intent;
 
 import com.actionbarsherlock.app.SherlockActivity;
@@ -11,7 +13,9 @@ import com.actionbarsherlock.app.SherlockActivity;
  */
 public abstract class BaseActivity extends SherlockActivity {
     
-    /**
+	public static final String TAG = BaseActivity.class.getSimpleName();
+
+	/**
      * Finishes the calling activity and launches the activity contained in
      * <code>intent</code>
      * @param intent
@@ -20,5 +24,43 @@ public abstract class BaseActivity extends SherlockActivity {
         startActivity(intent);
         finish();
     }
+    
+	// Session related 
+    public static final String INSTANCE_KEY = "instanceKey";
+    public static final String SESSION_KEY = "sessionKey";
+    
+    // instanceKey initialized to some random value for the instance;
+	private final String mInstanceKey = UUID.randomUUID().toString();
+	// Authenticated session key default is null;
+	private String mSessionKey = null;
+	
+	
+	/**
+	 * Returns the value of the instance key which is created when the object is
+	 * instantiated.
+	 * @return
+	 */
+    protected String getInstanceKey(){
+    	return mInstanceKey;
+    }
+	
+	/**
+	 * Returns the value of the session key. Warning: any key returned must be 
+	 * authenticated with the session service.
+	 * @return
+	 */
+    protected String getSessionKey(){
+    	return mSessionKey;
+    }
+    
+    /**
+     * Sets the value of the session key. Warning: this method does not make
+     * any atempt to validate whether the session is authenticated.
+     * @param sessionKey
+     */
+    protected void setSessionKey(String sessionKey){
+    	mSessionKey = sessionKey;
+    }
+    
     
 }
