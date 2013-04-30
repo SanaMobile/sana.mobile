9d02da2ce26519c12e0330fd47b9e0d272d31978
Eric Winkler <winkler.em@gmail.com>
2013-04-30 17:54:37 -0400

    Updated application state fields which get passed between components.
    Complete list now includes:
    instance key
    session key
    observer-i.e. user
    subject
    encounter
    procedure
    observation

--- app-android/src/org/sana/android/activity/BaseActivity.java
+++ app-android/src/org/sana/android/activity/BaseActivity.java
@@ -3,9 +3,13 @@ package org.sana.android.activity;
 import java.util.UUID;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
+import org.sana.android.app.State.Keys;
+import org.sana.android.util.UriUtil;
 /**
  * Base class that contains basic functionalities and behaviors that all
  * activities should do. 
@@ -30,10 +34,14 @@ public abstract class BaseActivity extends SherlockActivity {
     public static final String SESSION_KEY = "sessionKey";
     
     // instanceKey initialized to some random value for the instance;
-	private final String mInstanceKey = UUID.randomUUID().toString();
+	private String mInstanceKey = UUID.randomUUID().toString();
 	// Authenticated session key default is null;
 	private String mSessionKey = null;
-	
+
+    protected Uri mSubject = Uri.EMPTY;
+    protected Uri mEncounter = Uri.EMPTY;
+    protected Uri mProcedure = Uri.EMPTY;
+    protected Uri mObserver = Uri.EMPTY;
 	
 	/**
 	 * Returns the value of the instance key which is created when the object is
@@ -61,6 +69,170 @@ public abstract class BaseActivity extends SherlockActivity {
     protected void setSessionKey(String sessionKey){
     	mSessionKey = sessionKey;
     }
+     
+    /**
+     * Writes the state fields for this component to a bundle.
+     * Currently this writes the following from the Bundle
+     * <ul>
+     * 	<li>instance key</li>
+     * 	<li>session key</li>
+     * 	<li>current encounter</li>
+     * 	<li>current subject</li>
+     * 	<li>current observer</li>
+     * 	<li>current procedure</li>
+     * </ul>
+     * @param outState
+     */
+    protected void onSaveAppState(Bundle outState){
+    	outState.putString(Keys.INSTANCE_KEY, mInstanceKey);
+    	outState.putString(Keys.SESSION_KEY, mSessionKey);
+    	outState.putParcelable(Keys.ENCOUNTER, mEncounter);
+    	outState.putParcelable(Keys.SUBJECT, mSubject);
+    	outState.putParcelable(Keys.PROCEDURE, mProcedure);
+    	outState.putParcelable(Keys.OBSERVER, mObserver);
+    }
     
+    /**
+     * Writes the state fields for this component to an Intent as Extras.
+     * Currently this writes the following from the Intent.
+     * <ul>
+     * 	<li>instance key</li>
+     * 	<li>session key</li>
+     * 	<li>current encounter</li>
+     * 	<li>current subject</li>
+     * 	<li>current observer</li>
+     * 	<li>current procedure</li>
+     * </ul>
+     * @param outState
+     */
+    protected void onSaveAppState(Intent outState){
+    	outState.putExtra(Keys.INSTANCE_KEY, mInstanceKey);
+    	outState.putExtra(Keys.SESSION_KEY, mSessionKey);
+    	outState.putExtra(Keys.ENCOUNTER, mEncounter);
+    	outState.putExtra(Keys.SUBJECT, mSubject);
+    	outState.putExtra(Keys.PROCEDURE, mProcedure);
+    	outState.putExtra(Keys.OBSERVER, mObserver);
+    }
     
+    /**
+     * Sets the state fields for this component from an Intent.
+     * Currently this attempts to read the following extras from the
+     * Intent.
+     * <ul>
+     * 	<li>instance key</li>
+     * 	<li>session key</li>
+     * 	<li>current encounter</li>
+     * 	<li>current subject</li>
+     * 	<li>current observer</li>
+     * 	<li>current procedure</li>
+     * </ul>
+     * 
+     * @param inState
+     */
+    protected void onUpdateAppState(Intent inState){
+    	String k = inState.getStringExtra(Keys.INSTANCE_KEY);
+    	if(k != null)
+    		mInstanceKey = new String(k);
+
+		k = inState.getStringExtra(Keys.SESSION_KEY);
+    	if(k!=null)
+    		mSessionKey = new String(k);
+    	
+    	Uri temp = inState.getParcelableExtra(Keys.ENCOUNTER);
+    	if(temp != null)
+    		mEncounter = UriUtil.copyInstance(temp);
+    	
+    	temp = inState.getParcelableExtra(Keys.SUBJECT);
+    	if(temp != null)
+    		mSubject = UriUtil.copyInstance(temp);
+    	
+    	temp = inState.getParcelableExtra(Keys.PROCEDURE);
+    	if(temp != null)
+    		mProcedure = UriUtil.copyInstance(temp);
+    	
+    	temp = inState.getParcelableExtra(Keys.OBSERVER);
+    	if(temp != null)
+    		mObserver = UriUtil.copyInstance(temp);
+    }
+    
+    /**
+     * Sets the state fields for this component from a bundle.
+     * Currently this attempts to read the following from the Bundle
+     * <ul>
+     * 	<li>instance key</li>
+     * 	<li>session key</li>
+     * 	<li>current encounter</li>
+     * 	<li>current subject</li>
+     * 	<li>current observer</li>
+     * 	<li>current procedure</li>
+     * </ul>
+     * 
+     * @param inState
+     */
+    protected void onUpdateAppState(Bundle inState){
+    	String k = inState.getString(Keys.INSTANCE_KEY);
+    	if(k!=null)
+    		mInstanceKey = new String(k);
+    	k = inState.getString(Keys.SESSION_KEY);
+    	if(k!=null)
+    		mSessionKey = new String(k);
+    	
+    	Uri temp = inState.getParcelable(Keys.ENCOUNTER);
+    	if(temp != null)
+    		mEncounter = UriUtil.copyInstance(temp);
+    	
+    	temp = inState.getParcelable(Keys.SUBJECT);
+    	if(temp != null)
+    		mSubject = UriUtil.copyInstance(temp);
+    	
+    	temp = inState.getParcelable(Keys.PROCEDURE);
+    	if(temp != null)
+    		mProcedure = UriUtil.copyInstance(temp);
+    	
+    	temp = inState.getParcelable(Keys.OBSERVER);
+    	if(temp != null)
+    		mObserver = UriUtil.copyInstance(temp);
+    }
+
+    /**
+     * Writes the app state fields as extras to an Intent. Activities
+     * will still need to call setResult(RESULT_OK, data) as well as
+     * write any other data they wish to the data Intent.
+     * 
+     * @param data
+     */
+    protected void setResultAppData(Intent data){
+    	onSaveAppState(data);
+    }
+    
+    /*
+     * (non-Javadoc)
+     * @see com.actionbarsherlock.app.SherlockActivity#onSaveInstanceState(android.os.Bundle)
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState){
+    	onSaveAppState(savedInstanceState);
+    }
+    
+    /*
+     * (non-Javadoc)
+     * @see com.actionbarsherlock.app.SherlockActivity#onRestoreInstanceState(android.os.Bundle)
+     */
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+    	onUpdateAppState(savedInstanceState);
+    }
+    
+    /*
+     * (non-Javadoc)
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+    	super.onCreate(savedInstanceState);
+    	Intent intent = getIntent();
+    	// get the fields from the launch intent extras
+    	if(intent != null)
+    		onUpdateAppState(intent);
+    }
 }
