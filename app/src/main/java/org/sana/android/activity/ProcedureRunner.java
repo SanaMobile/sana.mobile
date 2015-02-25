3028df55718f691a285c6e984af91f8ac48478e0
Eric Winkler <winkler.em@gmail.com>
2015-02-24 20:18:06 -0500

    Implementation of more generalized broadcast handling.

--- app/src/main/java/org/sana/android/activity/ProcedureRunner.java
+++ app/src/main/java/org/sana/android/activity/ProcedureRunner.java
@@ -2,16 +2,23 @@
 package org.sana.android.activity;
 
 import org.sana.R;
+import org.sana.android.Constants;
 import org.sana.android.app.Locales;
+import org.sana.android.content.DispatchResponseReceiver;
 import org.sana.android.fragment.ProcedureRunnerFragment;
 import org.sana.android.fragment.BaseRunnerFragment.ProcedureListener;
 import org.sana.android.service.impl.InstrumentationService;
+import org.sana.android.util.Strings;
+import org.sana.net.Response;
 
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Menu;
+
 /** Activity which loops through the available steps within a procedure including
  * handling any branching logic. Individual procedure steps are rendered to a
  * view which is wrapped in a container which presents buttons for paging.
@@ -75,6 +82,21 @@ public class ProcedureRunner extends BaseRunner
     	}
     	super.onDestroy();
     }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        Locales.updateLocale(this, getString(R.string.force_locale));
+        menu.add(0, OPTION_SAVE_EXIT, 0, getString(R.string.menu_save_exit));
+        menu.add(0, OPTION_DISCARD_EXIT, 1, getString(R.string.menu_discard_exit));
+        menu.add(0, OPTION_VIEW_PAGES, 2, getString(R.string.menu_view_pages));
+        if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean(
+                Constants.PREFERENCE_EDUCATION_RESOURCE, false))
+            menu.add(0, OPTION_HELP, 3, "Help");
+        return true;
+    }
+
     @Override
     public void onProcedureComplete(Intent data){
         Log.d(TAG, "onProcedureComplete(): " + data);
@@ -87,5 +109,33 @@ public class ProcedureRunner extends BaseRunner
         Log.d(TAG, "onProcedureComplete(): " + message);
 		setResult(RESULT_CANCELED,null);
 		finish();
+    }
+
+    /**
+     * Handles a local broadcast response success. Subclasses should override
+     * this method to change behavior.
+     *
+     * @param intent The response message
+     */
+    protected void handleBroadcastResultSuccess(Intent intent){
+        Log.i(TAG, "handleBroadcastResultSuccess(Intent)");
+        String text = intent.hasExtra(Response.MESSAGE)?
+                intent.getStringExtra(Response.MESSAGE):
+                Strings.getLocalizedString(this,R.string.general_ok);
+        createUploadResultSuccessDialog(text).show();
+    }
+
+    /**
+     * Handles a local broadcast response failure. Subclasses should override
+     * this method to change behavior.
+     *
+     * @param intent The response message
+     */
+    protected void handleBroadcastResultFailure(Intent intent){
+        Log.i(TAG, "handleBroadcastResultFailure(Intent)");
+        String text = intent.hasExtra(Response.MESSAGE)?
+                intent.getStringExtra(Response.MESSAGE):
+                Strings.getLocalizedString(this,R.string.general_error);
+        createUploadResultFailDialog(text).show();
     }
 }
