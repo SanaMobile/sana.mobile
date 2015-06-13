2cccedc83a3e7f095a47b3358bf5b71a5a1ebe61
Eric Winkler <winkler.em@gmail.com>
2015-06-13 18:01:09 -0500

    Cleaned up and added docs.

--- api/src/main/java/org/sana/text/json/ISODateAdapter.java
+++ api/src/main/java/org/sana/text/json/ISODateAdapter.java
@@ -11,29 +11,32 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
-import javax.print.attribute.standard.MediaSize;
-
 /**
+ * ISO 8601 Date serializer.
+ *
  * Original Source: <a href="https://gist.github.com/bbirec/5748489">https://gist.github.com/bbirec/5748489</a>
  */
-public class ISODateAdapter implements JsonSerializer<Date>, JsonDeserializer<Date>{
+public class ISODateAdapter implements JsonSerializer<Date>,
+        JsonDeserializer<Date>{
+    /** The iso8601 date format used by the dispatch server */
+    public static final String ISO_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
     private final DateFormat iso8601Format;
 
     ISODateAdapter() {
-        //this.iso8601Format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss" +
-        //".SSS'Z'", Locale.US);
-        //this.iso8601Format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss" +
-        //        ".SSS'Z'", Locale.US);
-        this.iso8601Format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.US);
+        this.iso8601Format = new SimpleDateFormat(ISO_FORMAT, Locale.US);
         this.iso8601Format.setTimeZone(TimeZone.getTimeZone("UTC"));
     }
 
-    public JsonElement serialize(Date src, Type typeOfSrc, JsonSerializationContext context) {
+    @Override
+    public JsonElement serialize(Date src, Type typeOfSrc,
+                                 JsonSerializationContext context) {
         String dateFormatAsString = iso8601Format.format(src);
         return new JsonPrimitive(dateFormatAsString);
     }
 
-    public Date deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+    @Override
+    public Date deserialize(JsonElement json, Type typeOfT,
+                            JsonDeserializationContext context)
             throws JsonParseException {
         if (!(json instanceof JsonPrimitive)) {
             throw new JsonParseException("The date should be a string value");
@@ -46,10 +49,16 @@ public class ISODateAdapter implements JsonSerializer<Date>, JsonDeserializer<Da
         } else if (typeOfT == java.sql.Date.class) {
             return new java.sql.Date(date.getTime());
         } else {
-            throw new IllegalArgumentException(getClass() + " cannot deserialize to " + typeOfT);
+            throw new IllegalArgumentException(getClass() +
+                    " cannot deserialize to " + typeOfT);
         }
     }
 
+    /**
+     * Deserializer for date objects.
+     * @param json The JSON string to deserialize.
+     * @return a new Date instance.
+     */
     private Date deserializeToDate(JsonElement json) {
         try {
             return iso8601Format.parse(json.getAsString());
@@ -60,14 +69,19 @@ public class ISODateAdapter implements JsonSerializer<Date>, JsonDeserializer<Da
 
     private static final ISODateAdapter adapter = new ISODateAdapter();
 
+    /**
+     * Returns the singleton instance of the adapter.
+     *
+     * @return
+     */
     public static final ISODateAdapter get(){
         return adapter;
     }
 
-    class DateOBJ{
-        public Date dob;
-    }
     public static void main(String...args){
+        class DateOBJ{
+            public Date dob;
+        }
         String json = "{ 'dob':'2014-04-08T19:25:28'}";
         Type type = new TypeToken<DateOBJ>(){}.getType();
         Type dateType = new TypeToken<Date>(){}.getType();
