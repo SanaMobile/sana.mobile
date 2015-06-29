3f694cf4821fa6e495b74cd4e47fcf4075f56e0a
Eric Winkler <winkler.em@gmail.com>
2015-06-28 20:42:54 -0400

    Added client classes to hold transmission quality data

---
+++ api-android/src/main/java/org/sana/android/qos/QOSParcel.java
@@ -0,0 +1,61 @@
+package org.sana.android.qos;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import org.sana.android.util.Dates;
+import org.sana.qos.QOS;
+
+import java.util.Date;
+
+/**
+ * Parcelable representation of QOS object.
+ */
+public class QOSParcel extends QOS implements Parcelable {
+
+    public QOSParcel(){
+        super();
+    }
+
+    public QOSParcel(Parcel in){
+        source = in.readString();
+        target = in.readString();
+        sent = new Date(in.readLong());
+        received = new Date(in.readLong());
+        sendCount = in.readInt();
+        eventStart = new Date(in.readLong());
+        eventComplete = new Date(in.readLong());
+        requestComplete = new Date(in.readLong());
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(source);
+        parcel.writeString(target);
+        parcel.writeLong(sent.getTime());
+        parcel.writeLong(received.getTime());
+        parcel.writeInt(sendCount);
+        parcel.writeLong(eventStart.getTime());
+        parcel.writeLong(eventComplete.getTime());
+        parcel.writeLong(requestComplete.getTime());
+    }
+
+    public static Parcelable.Creator<QOSParcel> CREATOR = new Parcelable
+            .Creator<QOSParcel>(){
+
+        @Override
+        public QOSParcel createFromParcel(Parcel parcel) {
+            return new QOSParcel(parcel);
+        }
+
+        @Override
+        public QOSParcel[] newArray(int i) {
+            return new QOSParcel[i];
+        }
+    };
+}
---
+++ api/src/main/java/org/sana/qos/QOS.java
@@ -0,0 +1,23 @@
+package org.sana.qos;
+
+import java.util.Date;
+
+/**
+ * Transmission data sent from the client.
+ */
+public class QOS {
+    public String source = null;
+    public String target = null;
+    public Date sent = new Date();
+    public int sendCount = 1;
+    public Date eventStart = new Date();
+    public Date eventComplete = new Date();
+    public Date received = new Date();
+    public Date requestComplete = new Date();
+
+    public QOS(){}
+
+    public void increment(){
+        sendCount++;
+    }
+}
