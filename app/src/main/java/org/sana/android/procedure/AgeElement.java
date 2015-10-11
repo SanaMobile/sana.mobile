c6f1c379ac4fad166a6ead73cb7e19f2d0f49da1
Eric Winkler <winkler.em@gmail.com>
2015-10-11 19:41:29 -0400

    Adding element for entering age to calculate date of birth based on age

--- app/src/main/java/org/sana/android/procedure/AgeElement.java
+++ app/src/main/java/org/sana/android/procedure/AgeElement.java
@@ -1,17 +1,26 @@
 package org.sana.android.procedure;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
+import android.text.TextUtils;
 import android.text.method.DigitsKeyListener;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.NumberPicker;
 
+import org.joda.time.DateTime;
+import org.sana.R;
 import org.sana.util.DateUtil;
 import org.sana.util.Functions;
 import org.w3c.dom.Node;
 
 import java.text.ParseException;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -22,6 +31,7 @@ import java.util.Date;
 public class AgeElement  extends TextEntryElement {
     public static final String TAG = AgeElement.class.getSimpleName();
     Date dateAnswer = new Date();
+    NumberPicker mWidget;
     long age = 0;
     /**
      * Default constructor
@@ -48,14 +58,15 @@ public class AgeElement  extends TextEntryElement {
     public void setAnswer(String answer) {
         Log.i(TAG,"["+id+"]setAnswer(String)");
         this.answer = new String(answer);
+        try {
+            dateAnswer = DateUtil.parseDate(answer);
+            age = Functions.age(dateAnswer);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         if(isViewActive()) {
-            try {
-                dateAnswer = DateUtil.parseDate(answer);
-                age = Functions.age(dateAnswer);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-            et.setText(String.valueOf(age));
+            mWidget.setValue((int)age);
+            //et.setText(String.valueOf(age));
         }
         Log.d(TAG, "...answer='" + this.answer + "'");
         Log.d(TAG, "...calculated age='" + age + "'");
@@ -64,14 +75,9 @@ public class AgeElement  extends TextEntryElement {
     /** {@inheritDoc} */
     @Override
     public String getAnswer() {
-        Log.i(TAG,"["+id+"]getAnswer()");
-        String tempAnswer = null;
+        Log.i(TAG, "[" + id + "]getAnswer()");
         if(isViewActive()) {
-            // Need to be certain the answer value is stored
-            tempAnswer = (et.getText().length() == 0)? "":et.getText().toString();
-            int age = Integer.parseInt(tempAnswer);
-            dateAnswer = Functions.dateFromAge(age);
-            answer = DateUtil.format(dateAnswer);
+
         }
         Log.d(TAG,"...returning answer='" + answer + "'");
         return answer;
@@ -79,12 +85,45 @@ public class AgeElement  extends TextEntryElement {
 
     @Override
     protected View createView(Context c) {
+        LayoutInflater inflater = ((Activity)c).getLayoutInflater();
+        LinearLayout v = (LinearLayout)inflater.inflate(R.layout.widget_element_age, null);
+        mWidget = (NumberPicker) v.findViewById(R.id.answer);
+        mWidget.setMaxValue(120);
+        mWidget.setMinValue(0);
+        mWidget.setValue((int) age);
+        mWidget.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                    setAge(newVal);
+            }
+        });
+        /*
         et = new EditText(c);
         et.setText(String.valueOf(age));
         et.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT));
         et.setKeyListener(new DigitsKeyListener());
-        return encapsulateQuestion(c, et);
+        */
+        //return encapsulateQuestion(c, et);
+        return encapsulateQuestion(c, v);
+    }
+
+    public void setAge(long age){
+        Log.i(TAG, "[" + id + "]setAge()");
+        Log.d(TAG, "....age="+age);
+        this.age = age;
+        DateTime dateTime = new DateTime();
+        int month = dateTime.getMonthOfYear();
+        int day = dateTime.getDayOfMonth();
+        if(month < 6 || (month == 6 && day < 15)) {
+            dateAnswer = dateTime.minusYears((int)age + 1).withMonthOfYear(6)
+                    .withDayOfMonth(15).toLocalDate().toDate();
+        } else {
+            dateAnswer = dateTime.minusYears((int)age).withMonthOfYear(6)
+                    .withDayOfMonth(15).toLocalDate().toDate();
+        }
+        answer = DateUtil.format(dateAnswer);
+        Log.d(TAG, "....answer="+answer);
     }
 
     public static AgeElement fromXML(String id, String question,
