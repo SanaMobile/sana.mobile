abcab57a0c04398fb97ae080ced2ac86ffb3827c
Eric Winkler <winkler.em@gmail.com>
2016-05-11 17:02:53 -0400

    Added global Application class
    Begin prepping for moving current app state from  Activity to Application object.
    Handling in the Activity classes introduces too much redundant code.

---
+++ app/src/main/java/org/sana/android/app/Main.java
@@ -0,0 +1,15 @@
+package org.sana.android.app;
+
+import android.app.Application;
+import android.os.Bundle;
+
+/**
+ * Created by winkler.em@gmail.com, on 04/18/2016.
+ */
+public class Main extends Application {
+    public static final String TAG = Main.class.getSimpleName();
+
+    private Bundle mState = new Bundle();
+
+
+}
