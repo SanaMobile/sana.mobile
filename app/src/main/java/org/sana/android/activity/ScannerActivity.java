15cf5b93f3c163dfcd0eea66733e0fe598b53c43
Eric Winkler <winkler.em@gmail.com>
2016-06-10 20:47:54 -0400

    Barcode scanner activity

---
+++ app/src/main/java/org/sana/android/activity/ScannerActivity.java
@@ -0,0 +1,46 @@
+package org.sana.android.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+/**
+ * Created by winkler.em@gmail.com, on 06/08/2016.
+ */
+public class ScannerActivity extends Activity implements ZXingScannerView.ResultHandler {
+    public static final String TAG = ScannerActivity.class.getSimpleName();
+
+    private ZXingScannerView mScannerView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        // Do something with the result here
+        Log.v(TAG, result.getText()); // Prints scan results
+        Log.v(TAG, result.getBarcodeFormat().name()); // Prints the scan format (qrcode, pdf417 etc.)
+
+    }
+}
