5a72f51a5e677cf22b3d1a4b8fe1bcb4ab6e3ab3
David Su <dsu1995@yahoo.ca>
2018-02-07 23:40:20 -0500

    cleaned up firebase cloud messaging code

--- app/src/main/java/org/sana/android/service/messaging/ProcedureFirebaseInstanceIdService.java
+++ app/src/main/java/org/sana/android/service/messaging/ProcedureFirebaseInstanceIdService.java
@@ -1,27 +1,36 @@
 package org.sana.android.service.messaging;
 
-import android.util.JsonWriter;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.StringEntity;
-import org.json.JSONException;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by gil on 7/19/17.
- */
 
 public class ProcedureFirebaseInstanceIdService extends FirebaseInstanceIdService {
-    private static HttpPost registerUserRequest = new HttpPost("http://192.168.43.158:8000/api/devices");
-    private static String TAG = "GIL";
+    private static final String TAG = ProcedureFirebaseInstanceIdService.class.getName();
+
+    private static final String SERVER_URL = "http://192.168.43.158:8000/api/devices";
+    private static final HttpPost registerUserRequest = new HttpPost(SERVER_URL);
+
+    /**
+     * Called when the device specific token changes
+     */
     @Override
     public void onTokenRefresh() {
         // Get updated InstanceID token.
@@ -35,24 +44,49 @@ public class ProcedureFirebaseInstanceIdService extends FirebaseInstanceIdServic
         sendRegistrationToServer(refreshedToken);
     }
 
+    /**
+     * Register updated registration registration token to the SANA server
+     */
     private void sendRegistrationToServer(String registrationToken) {
         Map<String, String> userMap = new HashMap<>();
         userMap.put("registration_id", registrationToken);
-
-        JSONObject obj=new JSONObject(userMap);
+        JSONObject obj = new JSONObject(userMap);
 
         StringEntity se = null;
         try {
             se = new StringEntity(obj.toString());
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            Log.e(TAG, "UnsupportedEncodingException thrown: ", e);
         }
         registerUserRequest.setEntity(se);
         registerUserRequest.setHeader("Content-type", "application/json");
-        try {
-            Log.d(TAG, "response: " + new ExecuteRegisterIdWithServer().execute(registerUserRequest).get());
-        } catch (Exception e) {
-            Log.d(TAG, "response failed");
+
+        new ExecuteRegisterIdWithServer().execute(registerUserRequest);
+    }
+
+    /**
+     * Make HTTP request in background thread
+     */
+    private static class ExecuteRegisterIdWithServer extends AsyncTask<HttpRequest, Void, Integer> {
+        private HttpClient client = new DefaultHttpClient();
+
+        @Override
+        protected Integer doInBackground(HttpRequest... params) {
+            HttpRequest request = params[0];
+            try {
+                HttpResponse response = client.execute((HttpUriRequest)request);
+
+                Log.d(TAG, response.toString());
+                int responseCode = response.getStatusLine().getStatusCode();
+                Log.d(TAG, "response: " + responseCode);
+
+                return responseCode;
+            } catch (ClientProtocolException e) {
+                Log.e(TAG, "ClientProtocolException thrown: ", e);
+            } catch (IOException e) {
+                Log.e(TAG, "IOException thrown: ", e);
+            }
+            return -1;
         }
     }
 }
--- app/src/main/java/org/sana/android/service/messaging/ProcedureFirebaseMessagingService.java
+++ app/src/main/java/org/sana/android/service/messaging/ProcedureFirebaseMessagingService.java
@@ -6,33 +6,39 @@ import android.widget.Toast;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-/**
- * Created by gil on 7/19/17.
- */
+import java.util.Arrays;
+import java.util.Map;
 
 public class ProcedureFirebaseMessagingService extends FirebaseMessagingService {
-    static private String TAG = "GIL";
+    private static final String TAG = ProcedureFirebaseMessagingService.class.getName();
+
+    /**
+     * Called when Firebase sends a message to the Android device
+     */
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
 
-
-
+        Map<String, String> data = remoteMessage.getData();
 
         // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-//
-//            Toast.makeText(getBaseContext(), remoteMessage.getData().toString(),
-//                    Toast.LENGTH_LONG).show();
+        if (data.size() > 0) {
+            String dataString = Arrays.toString(data.entrySet().toArray());
+            Log.d(TAG, "Message data payload: " + dataString);
+
+            Toast.makeText(this, dataString, Toast.LENGTH_LONG).show();
 
             scheduleJob();
-
         }
     }
 
+    /**
+     * Message received contains information on how to ping the SANA API to
+     * fetch the updated procedure (i.e. URL, procedure ID, etc).
+     *
+     * This method calls the SANA API to fetch the updated procedure(s).
+     */
     private void scheduleJob() {
 
     }
